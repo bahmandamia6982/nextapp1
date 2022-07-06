65b82ddf39b43b3987dfa6f80c19fc1135a09241
Bahman World <bahman.world@gmail.com>
2022-07-06 21:56:29 +0430

    second

---
+++ pages/api/graphql.ts
@@ -0,0 +1,98 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { ApolloServer, gql } from "apollo-server-micro";
+import {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  AuthenticationError,
+} from "apollo-server-core";
+import Cors from "micro-cors";
+import User from "../../models/User";
+import mongoose from "mongoose";
+
+const connect = mongoose.connect(
+  "mongodb+srv://bahmandamia6982:bahmandamia6982@demo.wstts.mongodb.net/demo?retryWrites=true&w=majority"
+);
+
+const cors = Cors();
+
+const typeDefs = gql`
+  type Query {
+    token: String
+    getUsers(name: String!, skip: Float = 0, limit: Float! = 0): [User]
+  }
+  type Mutation {
+    createUser(name: String!, age: Float!): User!
+  }
+  type User {
+    name: String
+    age: Float
+  }
+`;
+
+const resolvers = {
+  Query: {
+    token: (parent: any, args: any, context: any) => {
+      return "eya4s9a4sx9a4s9x4asx";
+    },
+    getUsers: async (parent: any, args: any, context: any) => {
+      const nameRegex = new RegExp(args.name, "i");
+      const user = await User.find({ name: nameRegex })
+        .skip(args.skip)
+        .limit(args.limit);
+      return user;
+    },
+  },
+  Mutation: {
+    createUser: async (parent: any, args: any, context: any) => {
+      const user = new User({
+        name: args.name,
+        age: args.age,
+      });
+      await user.save();
+      return user;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  csrfPrevention: true,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  context: ({ req }) => {
+    return {};
+  },
+});
+
+const start = server.start();
+
+export default cors(
+  async (req: NextApiRequest | any, res: NextApiResponse | any) => {
+    try {
+      await connect;
+      console.warn("connected to mongodb");
+    } catch (error) {
+      console.warn("failed to onnect monodb");
+    }
+    try {
+      await start;
+      console.warn(
+        `connected to apollo server on http://localhost:3000/api/graphql`
+      );
+    } catch (error) {
+      console.warn("failed to connect to apollo server");
+    }
+    try {
+      await server.createHandler({ path: req.url })(req, res);
+      console.warn("apollo server handler created");
+    } catch (error) {
+      console.warn("failed to apollo server create handler");
+    }
+  }
+);
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import { GetServerSideProps } from "next";
+import mongoose from "mongoose";
 
 type Props = {
   soon?: boolean;
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
+
+
   return {
     props: {
       soon: false,
