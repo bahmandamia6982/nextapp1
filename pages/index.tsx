b6379e299a0742704bc631d69544c710cd96c098
Bahman World <bahman.world@gmail.com>
2022-07-07 17:46:56 +0430

    add custom favicon to site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,31 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import favicon from "../public/favicon.png";
 import { GetServerSideProps } from "next";
-import mongoose from "mongoose";
+import Head from "next/head";
 
 type Props = {
   soon?: boolean;
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
   return {
     props: {
-      soon: false,
-    } as Props,
+      soon: true,
+    },
   };
 };
 
 const Home: NextPage<Props> = (props) => {
+  console.log(favicon)
   return (
     <div className={styles.container}>
-      <div className="container">MODE = You are in {process.env.NODE_ENV || "none"} mode</div>
+      <Head>
+        <link rel="icon" type="image/png" href={favicon.src} />
+      </Head>
+      <div className="container">
+        MODE = You are in {process.env.NODE_ENV || "none"} mode
+      </div>
     </div>
   );
 };
