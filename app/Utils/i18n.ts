3c0d0be5a01dde46314abcd507e7f4356ddb3575
Bahman World <bahman.world@gmail.com>
2022-07-22 19:28:48 +0430

    some changes in nextjs

--- app/Utils/i18n.ts
+++ app/Utils/i18n.ts
@@ -1,6 +1,6 @@
 import i18next, { t } from 'i18next';
-import { translations as English } from '../languages/en';
-import { translations as Kurdish } from '../languages/ku';
+import { translations as English } from '../langs/en';
+import { translations as Kurdish } from '../langs/ku';
 import { initReactI18next } from 'react-i18next'; 
 
 export const initializeI18Next = async () => {
