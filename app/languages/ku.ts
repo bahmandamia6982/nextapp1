014e68897b49948f775fc6c31c7b4c026d9a14a3
Bahman World <bahman.world@gmail.com>
2022-07-22 19:25:44 +0430

    some changes in nextjs

---
+++ app/languages/ku.ts
@@ -0,0 +1,10 @@
+export const translations =  {
+  direction: 'rtl',
+  biog: 'بایوجی v{{version}}',
+  apple_zero: 'هیچ شیوێک نەدۆزرایەوە',
+  apple_one: 'یەکدانە سێو',
+  apple_other: '{{count}} سێو',
+  bahman: {
+    full_name: 'بهمن وۆرڵد',
+  },
+};
